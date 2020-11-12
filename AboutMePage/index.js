b7063595915e2baab142abbe7a35970fafd40853
AidenEyre <EyreAiden@gmail.com>
2020-11-12 15:36:37 -0700

    Added code to change body styles and innerHTML of each span element.
    Also made the JavaScript link load async in html.

--- AboutMePage/index.js
+++ AboutMePage/index.js
@@ -12,3 +12,16 @@
    
 */
 
+window.addEventListener("load", setup);
+
+function setup() {
+    // Change the font family for the body
+    document.body.style.fontFamily = "Arial, sans-serif";
+
+    // Update the information in each of the spans
+    document.getElementById("nickname").innerHTML = "Aiden";
+    document.getElementById("favorites").innerHTML = "skiing, hiking";
+    document.getElementById("hometown").innerHTML = "Brigham City";
+
+    
+}
