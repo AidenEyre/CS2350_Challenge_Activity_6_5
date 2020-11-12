1bb826d141c7994fc04562e1b8409d93446a5ff1
AidenEyre <EyreAiden@gmail.com>
2020-11-12 15:43:12 -0700

    using js DOM elements I changed the class and text color of each list element.

--- AboutMePage/index.js
+++ AboutMePage/index.js
@@ -18,10 +18,19 @@ function setup() {
     // Change the font family for the body
     document.body.style.fontFamily = "Arial, sans-serif";
 
+
     // Update the information in each of the spans
     document.getElementById("nickname").innerHTML = "Aiden";
     document.getElementById("favorites").innerHTML = "skiing, hiking";
     document.getElementById("hometown").innerHTML = "Brigham City";
 
-    
+
+    // Create a variable to hold each list element
+    let listLength = document.querySelectorAll("li");
+
+    // Loop through the list changing the class of each element to "listitem"
+    for (let i = 0; i < listLength.length; i++) {
+        listLength[i].setAttribute("class", "listitem");    // Set the class
+        listLength[i].style.color = "red";                  // Set the text color to red
+    }
 }
