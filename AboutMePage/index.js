7e03e776217321767808cf5f3d7d9605c68a0e14
AidenEyre <EyreAiden@gmail.com>
2020-11-12 14:57:44 -0700

    Initialized starter code for AboutMePage.

---
+++ AboutMePage/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*
+   6-5: Challenge: Manipulating the DOM
+
+   Author: Aiden Eyre
+   Date:   12 November 2020
+
+   Filename: index.js
+   
+   Write functions here *****
+   
+*/
