c9045c63d0b5e757877757cad8fca2b8bf0afbeb
AidenEyre <EyreAiden@gmail.com>
2020-11-12 18:34:09 -0700

    Created hikeTable() function that dynamically creates a table based off the the hike array.

--- MyCollection/index.js
+++ MyCollection/index.js
@@ -37,3 +37,44 @@ let hikes = [
      alreadyHiked: true
     }];
 
+function hikeTable() {
+    let headText = ["Hike", "Location", "Completed?"];
+
+    let table = document.createElement('table');
+
+    let tr = document.createElement('tr');
+    
+    for (let i = 0; i < headText.length; i++) {
+        let th = document.createElement('th');
+        let text = document.createTextNode(headText[i]);
+
+        th.appendChild(text);
+        tr.appendChild(th);
+    }
+    
+    table.appendChild(tr);
+    
+    for (let i = 1; i < hikes.length; i++){
+        let tr = document.createElement('tr');   
+
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+
+        let text1 = document.createTextNode(hikes[i].Hike);
+        let text2 = document.createTextNode(hikes[i].Location);
+        let text3 = document.createTextNode(hikes[i].alreadyHiked);
+
+        td1.appendChild(text1);
+        td2.appendChild(text2);
+        td3.appendChild(text3);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        table.appendChild(tr);
+    }
+
+    document.body.appendChild(table);
+
+}
